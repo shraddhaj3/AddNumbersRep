ba5ca894ad5a1e5e9e9d4112d87bddda998393a5
shraddhaj3 <shraddhaj3@users.noreply.github.com>
2017-11-27 12:45:18 -0600

    Update Operation.java
    Modify bug 1

--- src/com/opt/Operation.java
+++ src/com/opt/Operation.java
@@ -2,11 +2,11 @@ package com.opt;
 
 public class Operation {
 	public static int add(int a, int b) {
-		long result = ((long)a - (long)b);
+		long result = ((long)a + (long)b);
 		
 		//If addition result value overflows or underflows return 0
 		if(result > Integer.MAX_VALUE || result < Integer.MIN_VALUE)
-			return 0;
+			return 1;
 		
 		return (int)result;
 	}
